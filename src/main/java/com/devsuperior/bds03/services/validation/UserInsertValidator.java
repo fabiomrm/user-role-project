5df9a7c5f3d5c84d8d2e5c60b28f9b1fa1455b2e
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-02-23 11:46:25 -0300

    final commit

---
+++ src/main/java/com/devsuperior/bds03/services/validation/UserInsertValidator.java
@@ -0,0 +1,32 @@
+package com.devsuperior.bds03.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.devsuperior.bds03.controllers.exceptions.FieldMessage;
+import com.devsuperior.bds03.dto.UserDTO;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserDTO> {
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
